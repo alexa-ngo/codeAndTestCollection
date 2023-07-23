c8bd78ed88e5142c1289671fde6d1f6887b1bdc8
Alexa Ngo <alexaariango@outlook.com>
2023-07-23 05:07:38 -0700

    Word Pattern

---
+++ wordPatternLC290/wordPatternLC290.js
@@ -0,0 +1,32 @@
+function wordPattern(pattern, words) {
+
+    const map = new Map();
+    const seenWords = new Set();
+    const wordsArr = words.split(" ");
+    const patternArr = pattern.split("");
+
+    if (wordsArr.length !== patternArr.length) {
+        return false;
+    }
+
+    for (let i = 0; i < pattern.length; i++) {
+        const currChar = pattern.charAt(i);
+        if (!map.has(currChar) && !seenWords.has(wordsArr[i])) {
+            map.set(currChar, wordsArr[i]);
+            seenWords.add(wordsArr[i]);
+        } else if (!map.has(currChar) && seenWords.has(wordsArr[i])) {
+            return false;
+        } else if (map.has(currChar) && map.get(currChar) !== wordsArr[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+const pattern = "abba";
+const s = "dog cat cat dog";
+const result = wordPattern(pattern, s);
+console.log(result)
+
+
---
+++ wordPatternLC290/wordPatternLC290.test.js
@@ -0,0 +1,37 @@
+import { wordPattern } from "./wordPatternLC290";
+
+it('Returns true when pattern = "aaa" and s = "cat cat cat"', () => {
+    const pattern = "aaa";
+    const s = "cat cat cat";
+    const result = wordPattern(pattern, s);
+    expect(result).toBe(true);
+})
+
+it('Returns false when pattern = "abbabb" and s = "dog monkey cat dog dog dog"', () => {
+    const pattern = "abbabb";
+    const s = "dog monkey cat dog dog dog"
+    const result = wordPattern(pattern, s);
+    expect(result).toBe(false);
+})
+
+it('Returns true when pattern = "abba" and s = "dog cat cat dog"', () => {
+    const pattern = "abba";
+    const s = "dog cat cat dog"
+    const result = wordPattern(pattern, s);
+    expect(result).toBe(true);
+})
+
+it('Returns "" when pattern = "" and s = ""', () => {
+    const pattern = "";
+    const s = ""
+    const result = wordPattern(pattern, s);
+    expect(result).toBe(false);
+})
+
+
+it('Returns true when pattern = "abaa" and s = "dog cat dog dog"', () => {
+    const pattern = "abaa";
+    const s = "dog cat dog dog"
+    const result = wordPattern(pattern, s);
+    expect(result).toBe(true);
+})
