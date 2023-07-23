10ff95106ddaac8167646543e73b437624885a61
Alexa Ngo <alexaariango@outlook.com>
2023-07-23 05:28:02 -0700

    Updates

--- implementStackUsingQueuesLC225/implementStackUsingQueuesLC225.js
+++ implementStackUsingQueuesLC225/implementStackUsingQueuesLC225.js
@@ -1,4 +1,36 @@
+export class Stack {
+
+    constructor() {
+        this.queue1 = new Queue();
+        this.queue2 = new Queue();
+    }
+
+    push(item) {
+        return this.queue1.enqueue(item);
+    }
+
+    pop() {
+        return this.queue1.dequeue();
+    }
+
+    empty() {
+        return (this.queue1.isEmpty() && this.queue2.isEmpty())
+    }
+
+    top() {
+        while (!this.queue1.isEmpty()) {
+            const currItem = this.queue1.dequeue();
+            this.queue2.enqueue(currItem);
+        }
+        const temp = this.queue2.dequeue();
+        this.queue2.enqueue(temp);
+        return temp;
+    }
+};
+
+
 export class Queue {
+
     constructor() {
         this.items = [];
     }
@@ -8,15 +40,13 @@ export class Queue {
     }
 
     dequeue() {
-        return this.items.shift();
-    }
-
-    size() {
-        return this.items.length;
+        const workingItem = this.items.shift();
+        return workingItem;
     }
 
     isEmpty() {
         return this.items.length === 0;
     }
-}
+};
+
 
--- implementStackUsingQueuesLC225/implementStackUsingQueuesLC225.test.js
+++ implementStackUsingQueuesLC225/implementStackUsingQueuesLC225.test.js
@@ -1,4 +1,16 @@
-import { Queue } from "./implementStackUsingQueuesLC225";
+import { Stack, Queue } from "./implementStackUsingQueuesLC225";
+
+
+it('Dequeue three numbers from 1,2,3,4,5 to result in 4,5', () => {
+    const myQueue = new Queue();
+    myQueue.enqueue(1);
+    myQueue.enqueue(2);
+    myQueue.enqueue(3);
+    myQueue.enqueue(4);
+    myQueue.enqueue(5);
+    myQueue.dequeue();
+    expect(myQueue).toEqual({ "items": [2, 3, 4, 5] });
+})
 
 it('Test empty queue', () => {
     const myQueue = new Queue();
@@ -81,49 +93,86 @@ it('Returns false when the queue is not empty. The five values are dequeued', ()
     expect(result).toEqual(false);
 })
 
-it('Returns the size of the queue when there are two values', () => {
-    const myQueue = new Queue();
-    myQueue.enqueue(1);
-    myQueue.enqueue(2);
-    myQueue.enqueue(3);
-    myQueue.enqueue(4);
-    myQueue.enqueue(5);
-    myQueue.dequeue();
-    myQueue.dequeue();
-    myQueue.dequeue();
-    const result = myQueue.size();
-    expect(result).toEqual(2);
+it('Push 5,6,7 to queue1 when there are 2 existing items', () => {
+    const workingStack = new Stack();
+    workingStack.push(5);
+    workingStack.push(6);
+    workingStack.push(7);
+    expect(workingStack).toEqual({ "queue1": { "items": [5, 6, 7] }, "queue2": { "items": [] } })
 })
 
 
-it('Returns the size of the queue when there are two values', () => {
-    const myQueue = new Queue();
-    myQueue.enqueue(1);
-    myQueue.enqueue(2);
-    myQueue.enqueue(3);
-    myQueue.enqueue(4);
-    myQueue.enqueue(5);
-    myQueue.dequeue();
-    myQueue.dequeue();
-    myQueue.dequeue();
-    myQueue.dequeue();
-    myQueue.dequeue();
-    const result = myQueue.size();
-    expect(result).toEqual(0);
+it('Push 20, 301, 431 to queue1 when there are no existing items', () => {
+    const workingStack = new Stack();
+    workingStack.push(20);
+    workingStack.push(301);
+    workingStack.push(431);
+    expect(workingStack).toEqual({ "queue1": { "items": [20, 301, 431] }, "queue2": { "items": [] } })
 })
-// const myQueue = new Queue();
-// myQueue.enqueue(1);
-// myQueue.enqueue(2);
-// myQueue.enqueue(3);
-// myQueue.enqueue(4);
-// myQueue.enqueue(5);
-// myQueue.dequeue();
-// myQueue.dequeue();
-// myQueue.dequeue();
-// myQueue.dequeue();
-// myQueue.dequeue();
 
-// const sizeIt = myQueue.size();
-// const seeIt = myQueue.peek();
-// const isItEmpty = myQueue.isEmpty();
-// console.log(isItEmpty);
+it('Pop all of 5 items from the existing queue with 5 items', () => {
+    const workingStack = new Stack();
+    workingStack.push(20);
+    workingStack.push(301);
+    workingStack.push(431);
+    workingStack.push(76);
+    workingStack.push(98);
+    workingStack.pop();
+    workingStack.pop();
+    workingStack.pop();
+    workingStack.pop();
+    workingStack.pop();
+    expect(workingStack).toEqual({ "queue1": { "items": [] }, "queue2": { "items": [] } })
+})
+
+
+it('Pop 3 items from the existing queue with 7 items', () => {
+    const workingStack = new Stack();
+    workingStack.push(20);
+    workingStack.push(301);
+    workingStack.push(431);
+    workingStack.push(76);
+    workingStack.push(1);
+    workingStack.push(3);
+    workingStack.push(6);
+    workingStack.pop();
+    workingStack.pop();
+    workingStack.pop();
+    console.log(workingStack)
+    expect(workingStack).toEqual()
+})
+
+it('Return true when the queue is empty', () => {
+    const workingStack = new Stack();
+    workingStack.push(1);
+    workingStack.push(3);
+    workingStack.push(6);
+    workingStack.pop();
+    workingStack.pop();
+    workingStack.pop();
+    const result = workingStack.empty();
+    expect(result).toEqual(true)
+})
+
+it('Return false when the queue is empty', () => {
+    const workingStack = new Stack();
+    workingStack.push(1);
+    workingStack.push(3);
+    workingStack.push(6);
+    workingStack.pop();
+    workingStack.pop();
+    const result = workingStack.empty();
+    expect(result).toEqual(false)
+})
+
+it('Return 5 in when the queue is 5,7,8,9', () => {
+    const workingStack = new Stack();
+    workingStack.push(20);
+    workingStack.push(301);
+    workingStack.push(431);
+    workingStack.push(76);
+    workingStack.push(1);
+    const result = workingStack.top();
+    expect(result).toEqual(20);
+})
+
