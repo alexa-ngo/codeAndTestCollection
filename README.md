9c3f2d3333f8e8f06ae73aa12bbcfe879d65d241
Alexa Ngo <alexaariango@outlook.com>
2023-08-01 07:46:15 -0700

    Updates

--- README.md
+++ README.md
@@ -27,5 +27,6 @@ Here is an example how a coverage report looks like:
 - Island Perimeter LC 463  
 - Find All Disappeared Numbers LC 448: Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
 - Keyboard Row LC 500: Given an array of strings words, return the words that can be typed using letters of the alphabet on only one row of American keyboard.
+- Relative Ranks LC 506: Return an array answer of size n where answer[i] is the rank of the ith athlete.
 - Defanging an IP Address LC 1108: Given a valid (IPv4) IP address, return a defanged version of that IP address.
 - Armstrong Number LC 1134: Given an integer n, return true if and only if it is an Armstrong number. The k-digit number n is an Armstrong number if and only if the kth power of each digit sums to n.
--- isomorphicStringLC205/isomorphicStringLC205.js
+++ isomorphicStringLC205/isomorphicStringLC205.js
@@ -14,3 +14,48 @@ export function isoStr(s, t) {
     }
     return true;
 }
+
+// var findTheDifference = function(s, t) {
+//     let s1 = s.split('').sort();
+//     let t1 = t.split('').sort();
+//     for(let i = 0;i<t1.length;i++)
+//     {
+//         if(t1[i]!==s1[i]){
+//        return t1[i]}
+
+//     }
+
+// };var findTheDifference = function(s, t) {
+//     for (let letter of s)
+//         t = t.replace(letter, '');
+//     return t;
+// };
+
+
+let findTheDifference = function (s, t) {
+    let maps = new Map();
+    let mapt = new Map();
+    for (let i = 0; i < t.length; i++) {
+        if (mapt.has(t.charAt(i))) {
+            mapt.set(t.charAt(i), mapt.get(t.charAt(i)) + 1)
+
+        }
+        else {
+            mapt.set(t.charAt(i), 1);
+        }
+    }
+    for (let i = 0; i < s.length; i++) {
+        if (maps.has(s.charAt(i))) {
+            maps.set(s.charAt(i), maps.get(s.charAt(i)) + 1);
+
+        }
+        else {
+            maps.set(s.charAt(i), 1);
+        }
+    }
+    for (let [k, v] of mapt) {
+        if (v !== maps.get(k)) {
+            return k;
+        }
+    }
+};
