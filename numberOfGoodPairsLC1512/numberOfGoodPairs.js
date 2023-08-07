99da474736b5e26fe78dc4f2bce2ca0c5c50cb86
Alexa Ngo <alexaariango@outlook.com>
2023-08-07 08:56:52 -0700

    Number of Good Pairs

---
+++ numberOfGoodPairsLC1512/numberOfGoodPairs.js
@@ -0,0 +1,20 @@
+function numberGoodPairs(nums) {
+
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if ((nums[i] === nums[j]) && (i < j)) {
+                count++
+            }
+        }
+    }
+    return count;
+}
+
+
+const num = [1, 2, 3, 1, 1, 3]
+console.log(numberGoodPairs(num))
+
+
+
