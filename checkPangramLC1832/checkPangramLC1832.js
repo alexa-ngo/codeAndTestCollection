e38cc22dc93ead1e8b9de8ad5364117fd0956ee7
Alexa Ngo <alexaariango@outlook.com>
2023-08-08 05:42:46 -0700

    Check Pangram

---
+++ checkPangramLC1832/checkPangramLC1832.js
@@ -0,0 +1,19 @@
+function pangram(sentence) {
+
+    const set = new Set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+    console.log(set)
+
+    for (let i = 0; i < sentence.length; i++) {
+        if (set.has(sentence.charAt(i))) {
+            set.delete(sentence.charAt(i))
+        }
+    }
+    return set.size == 0;
+}
+
+//const sentence = "thequickbrownfoxjumpsoverthelazydog"
+const sentence = "leetcode"  // return false
+console.log(pangram(sentence))
+
+
