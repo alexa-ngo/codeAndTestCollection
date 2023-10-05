c634ad41516c448cf2137c45f04c1d9b86404bd5
Alexa Ngo <alexaariango@outlook.com>
2023-10-05 07:39:31 -0700

    Binary Game

--- binaryGame/binaryGame.js
+++ binaryGame/binaryGame.js
@@ -1,95 +1,38 @@
-// This is a Binary Conversion Game
+// Decimal to Hexadecimal converter
 
-// Convert Hexadecimal to Binary or Binary to Hexadecimal
-
-function binaryGame(typeOfGame, input) {  // either enter "binaryToHex" or "hexToBinary" and your user input respectively.
-    if (typeOfGame === "binaryToHex") {
-        return binaryToHex(input);
-    } else {
-        return hexToBinary(input);
-    }
-}
-
-function hexToBinary(data) {
-
-    const preInput = data.toUpperCase();
-    const input = preInput.replaceAll(" ", "");
+function decimalToHex(num, sourceBase) {
 
     let result = "";
+    let quotient = num;
 
-    const map = new Map([
-        ["0", "0000"],
-        ["1", "0001"],
-        ["2", "0010"],
-        ["3", "0011"],
-        ["4", "0100"],
-        ["5", "0101"],
-        ["6", "0110"],
-        ["7", "0111"],
-        ["8", "1000"],
-        ["9", "1001"],
-        ["A", "1010"],
-        ["B", "1011"],
-        ["C", "1100"],
-        ["D", "1101"],
-        ["E", "1110"],
-        ["F", "1111"]
-    ]);
+    while (quotient !== 0) {
+        const remainder = quotient % sourceBase;
+        quotient = Math.trunc(quotient / sourceBase);
 
-    for (let char of input) {
-        for (let _ of map) {
-            const currBinary = map.get(char);
-            result += currBinary + " ";
-            break;
-        }
-    }
-    console.log(result);
-}
-
-
-function binaryToHex(data) {
-    console.log(data)
-
-    const preInput = data;
-    console.log('pre', preInput)
-    const input = preInput.replaceAll(" ", "");
-    console.log('input', input)
-
-    let splitString = [];
-    let result = "";
-
-    for (let i = 0; i < data.length; i = i + 4) {
-        splitString.push(input.slice(i, i + 4));
-    };
-
-    const map = new Map([
-        ["0", "0000"],
-        ["1", "0001"],
-        ["2", "0010"],
-        ["3", "0011"],
-        ["4", "0100"],
-        ["5", "0101"],
-        ["6", "0110"],
-        ["7", "0111"],
-        ["8", "1000"],
-        ["9", "1001"],
-        ["A", "1010"],
-        ["B", "1011"],
-        ["C", "1100"],
-        ["D", "1101"],
-        ["E", "1110"],
-        ["F", "1111"]
-    ]);
-
-    for (let chunk of splitString) {
-        for (let [key, value] of map.entries()) {
-            if (value === chunk) {
-                result += key;
+        if (remainder < 10) {
+            result = remainder + result;
+        } else {
+            if (remainder === 10) {
+                result = "A" + result;
+            }
+            if (remainder === 11) {
+                result = "B" + result;
+            }
+            if (remainder === 12) {
+                result = "C" + result;
+            }
+            if (remainder === 13) {
+                result = "D" + result;
+            }
+            if (remainder === 14) {
+                result = "E" + result;
+            }
+            if (remainder === 15) {
+                result = "F" + result;
             }
         }
     }
-    console.log('the result', result);
+    return result;
 }
 
-
-const z = binaryGame("binaryToHex", "0011 1011 1001 1010 1100 1010"); //either enter "binaryToHex" or "hexToBinary"
+//console.log(decimalToHex(922337203685477, 16)); 
