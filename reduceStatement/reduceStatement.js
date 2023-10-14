f9736acad16850eb4837ee66343b1309d74dd71a
Alexa Ngo <alexaariango@outlook.com>
2023-10-14 12:46:16 -0700

    Update

--- reduceStatement/reduceStatement.js
+++ reduceStatement/reduceStatement.js
@@ -1,9 +1,6 @@
-// Goal: Reduce the names in the array into one string separated by new line character.
-// Use concat, reduce, and the new line character
+// Reduce the names in the array into one string separated by a new line character.
 
-
-let names = ["Annie", "Priscilla", "Shanna", "Sabrina", "Shirley"];
-let concatNames = names.concat();
-const allnames = concatNames.reduce((a, b) => a + "\n" + b);
+const names = ["Annie", "Priscilla", "Shanna", "Sabrina", "Shirley"];
+const allnames = names.reduce((a, b) => a + "\n" + b);
 console.log(allnames);
 
