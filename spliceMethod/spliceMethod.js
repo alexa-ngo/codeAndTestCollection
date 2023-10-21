6d969963a811e96a0901421e119b61aaab7908d0
Alexa Ngo <alexaariango@outlook.com>
2023-10-21 05:57:29 -0700

    Update

--- spliceMethod/spliceMethod.js
+++ spliceMethod/spliceMethod.js
@@ -9,14 +9,14 @@
 
 
 let names = ["Amy", "Ann", "Carmen", "Lynn", "Prissy", "Sabrina", "Shawn"]
-names.splice(3)     //  ["Lynn", "Prissy", "Sabrina", "Shawn"]; list is now ["Amy", "Ann", "Carmen"]
-names.splice(1, 2)  // ["Ann", "Carmen"]; list is now ["Amy"]
-names.splice(0, 1)  // ["Amy"]; list is now []
+names.splice(3)     //  removed values: ["Lynn", "Prissy", "Sabrina", "Shawn"]; list is now ["Amy", "Ann", "Carmen"]
+names.splice(1, 2)  // removed values: ["Ann", "Carmen"]; list is now ["Amy"]
+names.splice(0, 1)  // removed values: ["Amy"]; list is now []
 
 
 // The first two arguments to splice() specify which array elements are to be deleted.
 // Any arguments that come after are to be inserted into the array starting at the position specified by the first array.
 
 let girls = ["Julia", "Lynn", "Pris", "Shawny", "Shirley"]
-girls.splice(2, 2, "Alexa", "Mindy")        // []; girls is now ["Julia", "Lynn", "Alexa", "Mindy", "Shirley"]
-girls.splice(0, 1, ["Dayna", "Joey"], 5)    // ["Julia"]; girls is now [["Dayna", "Joey"], 5, "Lynn", "Alexa", "Mindy", "Shirley"];
+girls.splice(2, 2, "Alexa", "Mindy")        // removed values: []; girls is now ["Julia", "Lynn", "Alexa", "Mindy", "Shirley"]
+girls.splice(0, 1, ["Dayna", "Joey"], 5)    // removed values: ["Julia"]; girls is now [["Dayna", "Joey"], 5, "Lynn", "Alexa", "Mindy", "Shirley"];
