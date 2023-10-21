6f99380bd5a99bf08ed312d9bbacdbd4b1897fa7
Alexa Ngo <alexaariango@outlook.com>
2023-10-21 05:55:45 -0700

    Update

--- sliceMethod/sliceMethod.js
+++ sliceMethod/sliceMethod.js
@@ -8,11 +8,12 @@
 
 let list = ["a", "b", "c", "d"];
 
-list.slice(2);       // Expected ['c', 'd']
-list.slice(0, 3);    // Expected ['a', 'b', 'c']
+list.slice(2)         // Expected ['c', 'd']
+list.slice(0, -2)     // Expected ['a','b]
+list.slice(0, 3);     // Expected ['a', 'b', 'c']
 list.slice(1, -1);    // Expected ['b', 'c']
 list.slice(-2, -2);   // Expected []
 list.slice(-4, -3);   // Expected ['a']
 list.slice(-3, -2);   // Expected ['b']
-
+list.slice(-1, -3);   // Expected []
 
