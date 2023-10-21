55bb69ecda0c2d84d69b71f1db53312abc341265
Alexa Ngo <alexaariango@outlook.com>
2023-10-21 06:04:45 -0700

    Update

--- spliceMethod/spliceMethod.js
+++ spliceMethod/spliceMethod.js
@@ -1,6 +1,8 @@
 // Splice Method
 
 // Splice inserts or removes elements from an array. It modifies the array on which it is invoked.
+// A new array is created with a segment removed and/or replaced without mutating the original array, use toSpliced().
+// If we want to access a part of the array without modifying it, use slice().
 // It can delete, insert, or perform both operations at the same time. 
 // The first argument is the position where the insertion/deletion is to begin.
 // The second argmument is the length. If the second argument is omitted, 
@@ -9,9 +11,10 @@
 
 
 let names = ["Amy", "Ann", "Carmen", "Lynn", "Prissy", "Sabrina", "Shawn"]
-names.splice(3)     //  removed values: ["Lynn", "Prissy", "Sabrina", "Shawn"]; list is now ["Amy", "Ann", "Carmen"]
-names.splice(1, 2)  // removed values: ["Ann", "Carmen"]; list is now ["Amy"]
-names.splice(0, 1)  // removed values: ["Amy"]; list is now []
+const a = names.splice(3)     //  removed values: ["Lynn", "Prissy", "Sabrina", "Shawn"]; list is now ["Amy", "Ann", "Carmen"]
+const b = names.splice(1, 2)  // removed values: ["Ann", "Carmen"]; list is now ["Amy"]
+const c = names.splice(0, 1)  // removed values: ["Amy"]; list is now []
+
 
 
 // The first two arguments to splice() specify which array elements are to be deleted.
