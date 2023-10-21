8bcfbb3e380631258900e249ea95f3e52a8b6f0d
Alexa Ngo <alexaariango@outlook.com>
2023-10-20 22:41:02 -0700

    Slice Method

---
+++ sliceMethod/sliceMethod.js
@@ -0,0 +1,18 @@
+// Slice Method
+
+// slice returns the first argument and all subsequent elements up to, but not including,
+// the element specified by the second argument.
+
+// Using one argument: returns all elemnts from the start position to the end of the array.
+// Using one or two negative arguments:
+
+let list = ["a", "b", "c", "d"];
+
+list.slice(2);       // Expected ['c', 'd']
+list.slice(0, 3);    // Expected ['a', 'b', 'c']
+list.slice(1, -1);    // Expected ['b', 'c']
+list.slice(-2, -2);   // Expected []
+list.slice(-4, -3);   // Expected ['a']
+list.slice(-3, -2);   // Expected ['b']
+
+
