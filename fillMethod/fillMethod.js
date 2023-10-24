6587b920f72f6282559f0d826202f5fffa7834fa
Alexa Ngo <alexaariango@outlook.com>
2023-10-23 21:50:04 -0700

    Fill Method

---
+++ fillMethod/fillMethod.js
@@ -0,0 +1,45 @@
+// Fill Method
+
+// The fill method sets the elements of an array, or a slice of an array to what we specify it as.
+// Returns a mutated array.
+
+// first argument: the value to set array elements to.
+// second argument (optional): specifies the starting index. If it's omitted, filling starts at index 0.
+// third argument (optional): specifies the ending index of array elements up to by it doesn't not include it.
+//                            If the third argument is omitted, then the array is filled from the start index to the end.
+
+
+let a = new Array(6);               // Start with no elements and length 5
+a.fill("hi")                        // => [ 'hi', 'hi', 'hi', 'hi', 'hi', 'hi' ]; fill the array with "hi"
+
+
+// Now let's fill in the position of 1 to the end of the array with an animal object
+const animal = {
+    "name": "Dragon",
+    "color": "pink",
+    "age": 45
+}
+
+a.fill(animal, 1)
+// => ; fill with the animal object starting at index 1
+// [
+//     'hi',
+//     { name: 'Dragon', color: 'pink', age: 45 },
+//     { name: 'Dragon', color: 'pink', age: 45 },
+//     { name: 'Dragon', color: 'pink', age: 45 },
+//     { name: 'Dragon', color: 'pink', age: 45 },
+//     { name: 'Dragon', color: 'pink', age: 45 }
+//   ]
+
+
+// Now let's use the integer of 3 for index 3 to 5
+a.fill(3, 3, -2)
+// =>
+// [
+//     'hi',
+//     { name: 'Dragon', color: 'pink', age: 45 },
+//     { name: 'Dragon', color: 'pink', age: 45 },
+//     3,
+//     { name: 'Dragon', color: 'pink', age: 45 },
+//     { name: 'Dragon', color: 'pink', age: 45 }
+//   ]
