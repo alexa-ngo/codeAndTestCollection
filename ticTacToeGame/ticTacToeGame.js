621628df7003a86fd1e21c8e59f4a1c3c191b507
Alexa Ngo <alexaariango@outlook.com>
2023-11-01 23:03:52 -0700

    Tic-Tac-Toe Game

---
+++ ticTacToeGame/ticTacToeGame.js
@@ -0,0 +1,50 @@
+// Tic-Tac-Toe Game
+
+
+const prompt = require("prompt-sync")({ sigint: true });
+
+const p1name = prompt("Hi Player 1, may I have your name? ");
+const p2name = prompt("Hello Player 2, may I have your name? ");
+console.log();
+console.log(`Welcome Player 1: ${p1name}! (X)`);
+console.log(`Welcome Player 2: ${p2name}! (O)`);
+
+
+const winningCombo = ["X", "X", "X"]
+let playingBoard = [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"]];
+let turn = 1;
+
+console.log('playing board', playingBoard)
+
+
+function createPlayers() {
+
+    let player1 = p1name;
+    let player2 = p2name;
+
+    console.log("Player 1: ", player1, "(X)                ", "Player 2: ", player2, ("(O)"));
+
+    for (let i = 1; i < 8; i++) {
+        console.log();
+        if (turn === 1) {
+            const p1RowPosition = prompt(`${player1}, Please enter your row position: `);
+            const p1ColumnPosition = prompt(`${player1}, Please enter your column position: `);
+            playingBoard[p1RowPosition][p1ColumnPosition] = "X";
+            turn = 0;
+        } else {
+            const p2RowPosition = prompt(`${player2}, Please enter your row position: `);
+            const p2ColumnPosition = prompt(`${player2}, Please enter your column position: `);
+            playingBoard[p2RowPosition][p2ColumnPosition] = "O";
+            turn = 1;
+        }
+        console.log('playingboard after', playingBoard);
+
+    }
+
+    if (playingBoard[0] === winningCombo) {
+        console.log(`${player1} is the winner!`)
+    }
+
+};
+
+console.log(createPlayers());
